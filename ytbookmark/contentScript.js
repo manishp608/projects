d060fc8143058bdc786142eabf8cb197515974c8
manishp608 <manishpalavalasa@gmail.com>
2025-09-29 17:52:01 +0530

    started yt bookmark

---
+++ ytbookmark/contentScript.js
@@ -0,0 +1,19 @@
+(() => {
+    let youtubeLeftControls, youtubePlayer;
+    let currentVideo = "";
+
+    chrome.runtime.onMessage.addListener((obj, sender, response) => {
+        const { type, value, videoId } = obj;
+
+        if (type === "NEW") {
+            currentVideo = videoId;
+            newVideoLoaded();
+        }
+    });
+
+    const newVideoLoaded = () => {
+        const bookmarkBtnExists = document.getElementsByClassName("bookmark-btn")[0]
+
+        console.log(bookmarkBtnExists)
+    }
+})();
